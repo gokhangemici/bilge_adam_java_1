80643ea9fa88190c4f8bc52332329acd3b0c0adb
gokhangemici <gokhangemici93@gmail.com>
2021-09-15 16:31:06 +0300

    first commit

---
+++ src/com/bilgeadam/main/Addition.java
@@ -0,0 +1,19 @@
+package com.bilgeadam.main;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a1, a2, a3;
+		a1 = 4;
+		a2 = 5;
+		
+		a3 = a1 + a2;
+		System.out.println("Toplam: " + a3);
+		
+		
+		
+	}
+	
+}
---
+++ src/com/bilgeadam/main/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.bilgeadam.main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+		
+		
+		
+		
+	} 
+
+}
+
+
